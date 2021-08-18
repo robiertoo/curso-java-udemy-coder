2379c8fa3885af36f75fd98e0a7003faa973583c
Roberto <rob.nettom@gmail.com>
2021-08-18 20:54:42 -0300

    Finalizado Módulo 18 - JavaFX

--- exercicios-javafx/src/fxml/LoginControlador.java
+++ exercicios-javafx/src/fxml/LoginControlador.java
@@ -1,6 +1,9 @@
 package fxml;
 
+import org.controlsfx.control.Notifications;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -11,7 +14,21 @@ public class LoginControlador {
 	private PasswordField campoSenha;
 	
 	public void entrar() {
-		System.out.println(campoEmail.getText());
-		System.out.println(campoSenha.getText());
+		boolean emailValido = campoEmail.getText().equals("aluno@cod3r.com.br");
+		boolean senhaValida = campoSenha.getText().equals("123456");		
+		
+		if(emailValido && senhaValida) {
+			Notifications.create()
+				.position(Pos.TOP_RIGHT)
+				.title("Login FXML")
+				.text("Login efetuado com sucesso!")
+				.showInformation();
+		} else  {
+			Notifications.create()
+			.position(Pos.TOP_RIGHT)
+			.title("Login FXML")
+			.text("Usuário e Senha inválidos!")
+			.showError();
+		}
 	}
 }
